eab57a33799851d0a993bb0743a1a4e7ab802355
Robert Freigang <robertfreigang@gmx.de>
2022-11-16 12:15:39 +0100

    Use server side logic for voting on a mix

--- web/assets/controllers/vote-controller.js
+++ web/assets/controllers/vote-controller.js
@@ -1,23 +1,34 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static values = {
+        totalNumber: Number,
+        voteUrl: String,
+        // totalNumber: String,
+        // totalNumber: Boolean,
+        // totalNumber: Array,
+        // totalNumber: Object,
+    };
+
     static targets = ['total'];
 
     connect() {
         console.log('Hello stimulus');
+        console.log(this.totalNumberValue);
+        console.log(this.voteUrlValue);
     }
 
-    #changeVote(direction) { // private method in stimulus
-        let change;
-        if ('up' === direction) {
-            change = 1;
-        } else if ('down' === direction) {
-            change = -1;
-        } else {
-            throw `invalid direction "${direction}". Only "up" and "down" are valid.`;
-        }
-        const actualTotal = parseInt(this.totalTarget.textContent, 0);
-        this.totalTarget.textContent = actualTotal + change;
+    async #changeVote(direction) { // private method in stimulus
+        const response = await fetch(this.voteUrlValue, {
+            method: 'POST',
+            body: JSON.stringify({direction: direction}),
+        })
+        // without await
+        // const html = ....then(response => response.text())
+        // const html = ....then((response) => { return response.text() })
+
+        // this.element.innerHTML = await response.text();
+        this.totalTarget.innerHTML = await response.text();
     }
 
     upVote() {
