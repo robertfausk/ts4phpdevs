e2d395bd8a8715f2c8c1a7912f621c71444652b4
Robert Freigang <robertfreigang@gmx.de>
2022-11-16 16:39:36 +0100

    Reimplement the up/down voting with a live component

--- web/src/Twig/Components/MixVotingComponent.php
+++ web/src/Twig/Components/MixVotingComponent.php
@@ -3,7 +3,11 @@
 namespace App\Twig\Components;
 
 use App\Entity\VinylMix;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
@@ -18,4 +22,19 @@ final class MixVotingComponent
     #[LiveProp(writable: true)]
     public string $firstName = 'Karl';
 
+    #[LiveProp]
+    public bool $isVoted = false;
+
+    #[LiveAction] // component is also a service
+    public function vote(#[LiveArg] string $direction, EntityManagerInterface $em)
+    {
+        if ('up' === $direction) {
+            $this->mix->upVote();
+        } else {
+            $this->mix->downVote();
+        }
+//        $em->persist($this->mix); // persist is only needed for new objects
+        $em->flush();
+        $this->isVoted = true;
+    }
 }
