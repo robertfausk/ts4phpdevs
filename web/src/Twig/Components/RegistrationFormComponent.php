4f92a29ca03c67e05f0b3373e4dd6fb8b79ab469
Robert Freigang <robertfreigang@gmx.de>
2022-11-16 16:51:41 +0100

    Add live component RegistrationFormComponent

---
+++ web/src/Twig/Components/RegistrationFormComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent('registration_form')]
+final class RegistrationFormComponent
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    protected function instantiateForm(): FormInterface
+    {
+        // TODO: Implement instantiateForm() method.
+    }
+}
