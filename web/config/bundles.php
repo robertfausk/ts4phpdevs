6152448ec5a4f7c4881b69d3c0d07c5119176b94
Robert Freigang <robertfreigang@gmx.de>
2022-11-16 16:08:02 +0100

    Add plain symfony/ux-live-component

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -19,4 +19,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
