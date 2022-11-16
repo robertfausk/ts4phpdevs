9e1ff8ff7b0e2b5b1d4e1ee15071a9f2974f98dd
Robert Freigang <robertfreigang@gmx.de>
2022-11-16 16:22:53 +0100

    Reimplement the up/down voting with a live component
    simpliest example of live reloading with firstName

--- web/src/Twig/Components/MixVotingComponent.php
+++ web/src/Twig/Components/MixVotingComponent.php
@@ -2,11 +2,20 @@
 
 namespace App\Twig\Components;
 
+use App\Entity\VinylMix;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('mix_voting')]
 final class MixVotingComponent
 {
     use DefaultActionTrait;
+
+    #[LiveProp] // to have them automatically in all reloads
+    public VinylMix $mix;
+
+    #[LiveProp(writable: true)]
+    public string $firstName = 'Karl';
+
 }
